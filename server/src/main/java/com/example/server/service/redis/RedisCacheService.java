a2b277915de0f038dcf782caa272e7a248252a49
Aryan Rana <aryan.rana2005@gmail.com>
2025-08-27 17:39:56 +0530

    added cache service

---
+++ server/src/main/java/com/example/server/service/redis/RedisCacheService.java
@@ -0,0 +1,74 @@
+package com.example.server.service.redis;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class RedisCacheService {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+    
+    public <T> T getCache(String key, Class<T> entityClass) {
+        Object o = redisTemplate.opsForValue().get(key);
+        if (o == null) {
+            log.info("Object Null in Cache with key: {}", key);
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        log.info("Object as String from Cache for: {}", entityClass);
+        try {
+            return mapper.readValue((String) o, entityClass);
+        } catch (Exception ex) {
+            log.error("Failed to PARSE OBJECT: " + ex.getMessage());
+        }
+        return null;
+    }
+    public <T> T getCache(String key, TypeReference<T> typeReference) {
+        Object o = redisTemplate.opsForValue().get(key);
+        if (o == null) {
+            log.info("Object Null in Cache with key: {}", key);
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        log.info("Object as String from Cache for: {}", typeReference);
+        try {
+            return mapper.readValue((String) o, typeReference);
+        } catch (JsonProcessingException ex) {
+            log.error("Failed to PARSE OBJECT: " + ex.getMessage());
+        }
+        return null;
+    }
+
+    public void setCache(String key, Object o, Integer ttl) {
+        try {
+            String jsonValue = new ObjectMapper().writeValueAsString(o);
+            redisTemplate.opsForValue().set(key, jsonValue, ttl, TimeUnit.MINUTES);
+        } catch (Exception ex) {
+            log.info("Failed to SAVE in cache: " + ex.getMessage());
+        }
+    }
+
+    public void deleteCache(String key) {
+        try {
+            redisTemplate.delete(key);
+        } catch (Exception ex) {
+            log.error("Failed to DELETE cache: " + ex.getMessage());
+        }
+        log.warn("DELETED " + key + " from cache");
+    }
+}
