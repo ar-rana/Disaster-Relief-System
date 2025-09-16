34fd06cf418288b573063f6350998eec9ccb2820
Aryan Rana <aryan.rana2005@gmail.com>
2025-09-16 18:19:38 +0530

    ReliefStatus repo and Relief controller added

--- server/src/main/java/com/example/server/contoller/ReliefController.java
+++ server/src/main/java/com/example/server/contoller/ReliefController.java
@@ -3,7 +3,10 @@ package com.example.server.contoller;
 import java.util.Map;
 
 import com.example.server.model.ReliefReq;
+import com.example.server.model.enums.Criticality;
+import com.example.server.service.rabbit.Producer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +19,18 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class ReliefController {
 
+    @Autowired
+    private Producer publish;
+
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody ReliefReq item) {
         log.info("Relief request received: {}", item.toString());
+//        item.setCriticality(Criticality.BASIC);
+//        publish.sendMessage(item, Criticality.BASIC);
+//        publish.sendMessage(item, Criticality.BASIC);
+//        publish.sendMessage(item, Criticality.BASIC);
+//        item.setCriticality(Criticality.VERY_HIGH);
+//        publish.sendMessage(item, Criticality.VERY_HIGH);
         return ResponseEntity.ok().build();
     }
 
