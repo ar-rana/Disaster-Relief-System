6bff134350cba000db842583a17c7839a93fcbd7
Aryan Rana <aryan.rana2005@gmail.com>
2025-09-16 18:20:01 +0530

    UserService added

---
+++ server/src/main/java/com/example/server/service/UserService.java
@@ -0,0 +1,57 @@
+package com.example.server.service;
+
+import java.util.Optional;
+
+import com.example.server.model.User;
+import com.example.server.model.enums.Keys;
+import com.example.server.repository.UserRepository;
+import com.example.server.service.redis.RedisCacheService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private RedisCacheService cache;
+
+    public String createUser(String name, String username, String password) {
+        User existingUser = getUserByUsername(username);
+        if (existingUser != null) {
+            return "User Already Exists!!";
+        }
+        User provider = new User(username, name, username, password); // username is also the number
+        User savedUser = repository.save(provider);
+        cache.setCache(Keys.key(Keys.PROVIDER, savedUser.getUsername()), savedUser, 120);
+
+        return "User registered Successfully";
+    }
+
+    public String login(String username, String password) {
+        User existingUser = getUserByUsername(username);
+        if (existingUser == null) {
+            return "User Does Not Exist!!";
+        }
+        return "";
+    }
+
+    public User getUserByUsername(String username) {
+        String key = Keys.key(Keys.PROVIDER, username);
+        User item = cache.getCache(key, User.class);
+        if (item != null) {
+            log.info("getUserByUsername from cache: {}", item);
+            return item;
+        }
+        Optional<User> user = repository.findById(username);
+        if (user.isPresent()) {
+            cache.setCache(key, user.get(), 120);
+            return user.get();
+        }
+        return null;
+    }
+}
